c933fa92ae879fc0b8cadd1d9eabbd932ab8ba8b
kxGeou <grzegorzm.work@gmail.com>
2025-03-02 11:56:22 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+
+function App() {
+
+
+  return (
+    <main>
+        <TodoInput></TodoInput>
+        <TodoList></TodoList>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/TodoInput.tsx
@@ -0,0 +1,8 @@
+export default function TodoInput() {
+  return (
+    <header>
+        <input placeholder="Enter todo..."></input>
+        <button>Add</button>    
+    </header>
+  )
+}
